d7c417d2204ca97dc8e88595e4dfabaa5e28765c
MonoGot <kirati.samat@gmail.com>
2022-06-03 15:59:32 +0700

    feat: add Whitlist contract

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,4 +1,8 @@
 require("@nomiclabs/hardhat-waffle");
+require("dotenv").config({ path: ".env" });
+
+const ALCHEMY_API_KEY_URL = process.env.ALCHEMY_API_KEY_URL;
+const GOERLI_PRIVATE_KEY = process.env.GOERLI_PRIVATE_KEY;
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
@@ -18,4 +22,10 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
  */
 module.exports = {
   solidity: "0.8.4",
+  networks: {
+    goerli: {
+      url: ALCHEMY_API_KEY_URL,
+      accounts: [GOERLI_PRIVATE_KEY],
+    }
+  }
 };
